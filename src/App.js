e8ba75315abd35e66c1c64f99320f171fec938ef
Oscar D McCullough <odm3productions@gmail.com>
2021-01-09 17:58:16 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hosted by Github Pages at <a href="http://odm3.github.io/samplereactapp">This link</a>
         </p>
         <a
           className="App-link"
